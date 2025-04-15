2d1ca6b667d238fb2bc600c8a74a497ab2e9df46
Marissa Borges <marissaborges2006@gmail.com>
2025-04-15 18:33:54 -0300

    Aula 1 da Imersão Dev 2025, Conversor de Moeda

--- Aula001/script.js
+++ Aula001/script.js
@@ -1,3 +1,5 @@
-valor = prompt("Digite um valor em dólar: ");
-dolar = 5.76;
-alert("O valor convertido para reais é  R$" + valor * dolar);
+function conversor() {
+  valor = prompt("Digite um valor em dólar: ");
+  dolar = 5.76;
+  alert("O valor convertido para reais é  R$" + valor * dolar);
+}
